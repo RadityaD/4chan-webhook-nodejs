abae438ee532c059dedee33b866b01df57d5b92f
radityad <raditya.danang@gmail.com>
2017-02-27 23:55:23 +0700

    ganti header biar ga standar

--- src/App.js
+++ src/App.js
@@ -8,13 +8,9 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>4Chan Webhook Node Js Version</h1>
         </div>
         <BoardSelection />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> Berak and save to reload.
-        </p>
       </div>
     );
   }
